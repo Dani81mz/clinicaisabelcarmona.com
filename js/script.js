4f9b842799105bcf3d195590ef7ca16368dbc8cf
Dani81mz <danielnessfotografia@gmail.com>
2022-07-08 10:54:54 +0200

    primer commit Isabel

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .nav');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+     header.classList.add('active');
+  }else{
+     header.classList.remove('active');
+  }
+
+}
+
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+// function openFulImg(reference){
+//    fulImgBox.style.display = "flex";
+//    fulImg.src = reference
+// }
+
+function closeImg(){
+   fulImgBox.style.display = "none";
+}
